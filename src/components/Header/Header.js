e6df121a1cd0104acb8ed7b78e96c1584e0c0823
Sarai Escobar <saraiee17@yahoo.com>
2023-03-16 18:00:55 -0500

    feat:header improved flex for logo location

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,11 @@ function Header(){
 
     return(
         <div className='header'>
-                <span class="material-symbols-rounded">menu</span>
-                <h1 className='header__title'>Villa for Sale in Tulum</h1>
                 <div className='header__flex'>
+                    <span class="material-symbols-rounded">menu</span>
+                    <h1 className='header__title'>Villa for Sale in Tulum</h1>
+                </div>
+                <div className='header__navflex'>
                     <p className='header__nav'>Home</p>
                     <p className='header__nav'>Contact</p>
                 </div>
