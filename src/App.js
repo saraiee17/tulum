708e30004d9f1ca650e255c0a74b8747f1a86c9d
Sarai Escobar <saraiee17@yahoo.com>
2023-11-02 20:07:45 -0500

    updated images

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import './App.scss';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
 import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
-        <Header/>
         <Routes>
-        <Route path='/' element ={<Home/>}/>
-        <Route path='/Contact' element ={<Contact/>} />
+        <Route
+              path='/'
+              element={
+                <div>
+                  <Header />
+                  <Home />
+                </div>
+              }
+            />
+            <Route
+              path='/Contact'
+              element={
+                <div>
+                  <Header />
+                  <Contact />
+                </div>
+              }/>
+        <Route path= '/modal' element = {<Nav/>}/>
         </Routes>
       </BrowserRouter>
         
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,16 +1,23 @@
 import './Contact.scss'
+import icon from '../../assets/whatsapp.svg'
+import location from '../../assets/Location.png'
 
 
 function Contact() {
     return (
       <div className="contact">
-        <h2 className="contact-title">CONTACT US FOR INQUIRIES & DETAILS</h2>
-        <h3 className="contact-email">xunkarivilla@gmail.com</h3>
-        <button className="contact-button">Reach out on Whatsapp</button>
+        <h2 className="contact__title">CONTACT US FOR INQUIRIES & DETAILS</h2>
+        <h3 className="contact__email">xunkarivilla@gmail.com</h3>
+        <button className="contact__button"><img className="contact__button--logo" src={icon} alt='whatsapplogo'/>Reach out on Whatsapp</button>
         <h3>XunKari Villa for Sale in Tulum</h3>
         <p>Tulum, Quintana Roo, Mexico</p>
 
-        <h2>mapbox</h2>
+        <img className='contact__map' src={location} alt='map'/>
+
+        <div>
+          
+          <button> Directions </button>
+        </div>
       </div>
     );
   }
